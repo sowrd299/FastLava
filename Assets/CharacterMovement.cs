335593e322d829fea22b891599121821e8499d8f
destintrang <destintrang@gmail.com>
2016-10-14 23:22:26 -0700

    Character movement
    movement for the character

---
+++ Assets/CharacterMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovement : MonoBehaviour {
+
+	public float playerSpeed = 10;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Movement ();
+	}
+
+	void Movement() {
+		float translation = Time.deltaTime * playerSpeed;
+
+		if (Input.GetKey("w"))
+			transform.Translate(0, translation, 0);
+		if (Input.GetKey("a"))
+			transform.Translate(-translation, 0, 0);
+		if (Input.GetKey("s"))
+			transform.Translate(0, -translation, 0);
+		if (Input.GetKey("d"))
+			transform.Translate(translation, 0, 0);
+	}
+}
