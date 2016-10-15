9650cbe2c0bbee4f5b09edd50dfeb4afc0971c97
Jerson Villanueva <jersonv@uci.edu>
2016-10-15 10:03:20 -0700

    Basic Rage Bar

---
+++ Assets/UI/Rage_Bar.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Rage_Bar : MonoBehaviour {
+
+    public GameObject player;
+    public Slider rage_bar;
+    
+    double Calculate_Rage()
+    {
+        return 50.0;
+    }
+
+    void Start ()
+    {
+        rage_bar.maxValue = 100;
+        rage_bar.minValue = 0;
+        rage_bar.value = 0;
+        
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //Player's Rage Amount 
+        rage_bar.value = Calculate_Rage();
+        
+	}
+}
