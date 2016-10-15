ddc011af633abef091d2a7cfc99f5acae1023dca
sowrd299 <ryan.ward46@gmail.com>
2016-10-15 09:39:18 -0700

    basic enemy added

---
+++ Assets/EnviormentScripts/Killable.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Killable : MonoBehaviour {
+
+    public int rageVal = 10; //public for unity; do not access
+    virtual public int RageVal
+    {
+        get
+        {
+            return rageVal; 
+        }
+    }
+
+    virtual public bool Hit()
+    {
+        ///To be called when hit by player durring dash.
+        ///Returns true if killed.
+        Destroy(gameObject);
+        return true;
+    }
+
+}
