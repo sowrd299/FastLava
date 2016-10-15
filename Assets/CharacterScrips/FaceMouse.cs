162e0bd60fad73f4618076c2d6be48273dc1d91a
sowrd299 <ryan.ward46@gmail.com>
2016-10-15 01:37:43 -0700

    Add rotating player sprite

---
+++ Assets/CharacterScrips/FaceMouse.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System;
+
+public class FaceMouse : MonoBehaviour {
+	
+    // a script that causes the attached object to rotate to face the mouse
+
+	void Update () {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2((mousePos.y - transform.position.y), (mousePos.x - transform.position.x)) * Mathf.Rad2Deg - 90);
+	}
+}
