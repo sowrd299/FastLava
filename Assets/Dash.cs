07ebfcb3fa7033c690c826ac4e91569fb7b76905
Kyle Wong <kylewonga@gmail.com>
2016-10-14 23:33:09 -0700

    Dash-Added dash method and enemy collision detection
    Lets anyone call a dash() and creates a polygon2DCollider to get all
    collisions along dash path.  This is all untested, very likely to fail

--- Assets/Dash.cs
+++ Assets/Dash.cs
@@ -10,22 +10,10 @@ public class Dash : MonoBehaviour {
     public float duration;
     public GameObject player;
     private float dashTimer;
-    private float dashAngle;
+    private float dashDirection;
     private Vector2 moveVector;
-    private GameObject[] enemyList;
 	void Start () {
-        dashTimer = 0;
-
-
-        float xDiff = Input.mousePosition.x - player.transform.position.x;
-        float yDiff = Input.mousePosition.y - player.transform.position.y;
-        dashAngle = getAngle(xDiff, yDiff);
-
-
-        float stepDistance = dashDistance * (dashTimer / duration);   //div by zero if duration is zero
-        moveVector = new Vector2(stepDistance * Mathf.Cos(dashAngle), stepDistance * Mathf.Sin(dashAngle));
-        enemyList = getCollisions();
-        killEnemies(enemyList);
+        dash(0, 0, 0); //instantiates variables
     }
 	
 	// Update is called once per frame
@@ -33,16 +21,34 @@ public class Dash : MonoBehaviour {
 	    if(dashTimer < duration)
         {
             dashTimer += Time.deltaTime;
-            player.transform.Translate(moveVector*Time.deltaTime);            
+            player.transform.Translate(moveVector*Time.deltaTime);     //Not sure I'm using time.deltatime correctly here       
         }
 	}
 
-    
+    public void dash(int dashDistance, float dashDirection, float duration)        //call this to dash in a direction for a distance in a certain time.  
+    {                                                                              //duration = 1 for a single frame dash
+        dashTimer = 0;
+        
+        float xDiff = Input.mousePosition.x - player.transform.position.x;
+        float yDiff = Input.mousePosition.y - player.transform.position.y;
+        dashDirection = getAngle(xDiff, yDiff);
+
+
+        float stepDistance = dashDistance * (dashTimer / duration);   //div by zero if duration is zero
+        moveVector = new Vector2(stepDistance * Mathf.Cos(dashDirection), stepDistance * Mathf.Sin(dashDirection));
+
+        RectTransform playerRect = (RectTransform)player.transform;
+        int playerWidth = (int)playerRect.rect.width;
+
+        ArrayList enemyList = getCollisions((int)dashDistance, dashDirection, playerWidth);
+        killEnemies(enemyList);
+    }
 
     
 
     public PolygonCollider2D getPolygon(int dashDistance, float dashDirection, int playerWidth)
-    {
+    {  //return a rectangular polygon that encompasses the entire dash movement
+
         Vector2[] pointList = new Vector2[4];
 
         //player's right side before dash, assuming facing up
@@ -72,21 +78,35 @@ public class Dash : MonoBehaviour {
 
     }
 
-    public GameObject[] getCollisions(int dashDistance, float dashDirection, int playerWidth)
+    public ArrayList getCollisions(int dashDistance, float dashDirection, int playerWidth)  //return a list of all affected enemies
     {
 
         PolygonCollider2D poly = getPolygon(dashDistance,dashDirection,playerWidth);
-        
 
-        GameObject[] result;
-        enemyList = GameObject.FindGameObjectsWithTag("Enemy");
-        for(int i = 0; i < enemyList.Length; i++)
+
+        ArrayList result = new ArrayList();
+        
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        for(int i = 0; i < enemies.Length; i++)
         {
             //if(poly.IsTouching())  need a way to reference enemy's Collider2D
+            
         }
         return result;
 
     }
+    public void killEnemies(ArrayList enemyList)
+    {
+        for (int i = 0; i < enemyList.Count; i++)
+        {
+            print("ENEMY WAS HIT");
+            //TELL ENEMIES TO KILL THEMSELVES
+        }
+    }
+
+
+
+
     private float polarX(float distance, float angle)  //returns x coord of a distance in a given direction
     {  
         return (float)(distance * System.Math.Cos(angle));  
@@ -102,14 +122,8 @@ public class Dash : MonoBehaviour {
         return (float)(System.Math.Atan2(yDiff, xDiff) * 180.0 / System.Math.PI);
     }
 
-    public void killEnemies(GameObject[] enemyList)
-    {
-        for(int i = 0; i < enemyList.Length; i++)
-        {
-            print("ENEMY WAS HIT");
-            //TELL ENEMIES TO KILL THEMSELVES
-        }
-    }
+
+    
    
 
 }
