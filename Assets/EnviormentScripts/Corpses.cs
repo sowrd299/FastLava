585c5da2bce1aeb5a46f8756c5105a1f3fe5dead
Jerson Villanueva <jersonv@uci.edu>
2016-10-16 13:05:38 -0700

    Corpes fixes

---
+++ Assets/EnviormentScripts/Corpses.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Corpses : MonoBehaviour {
+
+    Camera cam;
+
+    void Start()
+    {
+        GameObject tempObject = GameObject.FindGameObjectWithTag("MainCamera");
+        cam = tempObject.GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        Vector3 screenPos = cam.WorldToViewportPoint(transform.position);
+        if (!(screenPos.x < 1 && screenPos.x > 0 && screenPos.y < 1 && screenPos.y > 0))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
