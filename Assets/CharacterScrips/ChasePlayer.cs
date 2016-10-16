7f4303dd6646d98f97ab785cf385c4a7c57d93d6
Kyle Wong <kylewonga@gmail.com>
2016-10-15 22:02:54 -0700

    ChasePlayer is complete

--- Assets/CharacterScrips/ChasePlayer.cs
+++ Assets/CharacterScrips/ChasePlayer.cs
@@ -3,13 +3,42 @@ using System.Collections;
 
 public class ChasePlayer : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    private GameObject player;
+    private Vector2 playerLocation;
+    private float angleToPlayer;
+    private float distanceToPlayer;
+    public float runRadius;
+    public float speed;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        angleToPlayer = getAngleToPlayer();
+        runRadius = (float)(runRadius + Random.value * 3 - 1.5);
+        speed = (float)(speed + Random.value * 0.45 - 0.9);
+
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+        if (distanceToPlayer < runRadius)
+        {
+            angleToPlayer = getAngleToPlayer();
+            transform.Translate((speed * Mathf.Cos(angleToPlayer)) * Time.deltaTime, (speed * Mathf.Sin(angleToPlayer )) * Time.deltaTime, 0);
+
+        }
+
+    }
+    public float getAngleToPlayer()  //returns the angle from a given xDiff and yDiff
+    {  //returns radian
+        playerLocation = player.transform.position;
+        float xDiff = playerLocation.x - transform.position.x;
+        float yDiff = playerLocation.y - transform.position.y;
+
+        return (float)(System.Math.Atan2(yDiff, xDiff));
+    }
 }
