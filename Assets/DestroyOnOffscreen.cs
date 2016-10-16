7ae4c5f5a56cf17c7d4c2e29761f19cbe72d9b53
Kyle Wong <kylewonga@gmail.com>
2016-10-16 12:58:42 -0700

    Created flame animation and generic destroyOnOffScreen script

---
+++ Assets/DestroyOnOffscreen.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyOnOffscreen : MonoBehaviour {
+
+    // Use this for initialization
+    Camera cam;
+    void Start () {
+        GameObject tempObject = GameObject.FindGameObjectWithTag("MainCamera");
+        cam = tempObject.GetComponent<Camera>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 screenPos = cam.WorldToViewportPoint(transform.position);
+        if (!(screenPos.x < 1 && screenPos.x > 0 && screenPos.y < 1 && screenPos.y > 0))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
